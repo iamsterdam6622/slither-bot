f62d083d60e7d86acb2ff15afd8f01204d2a3bc8
iamsterdam6622 <iamsterdam6622@gmail.com>
2026-08-18 16:45:20 +0200

    Integrate Flask server to keep bot alive
    Added Flask server to keep the bot alive and modified the bot startup process.

--- Bot.py
+++ Bot.py
@@ -1,3 +1,18 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+Thread(target=run).start()
 import discord
 from discord.ext import commands
 
